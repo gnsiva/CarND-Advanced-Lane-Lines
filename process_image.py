b9cb292249efc1e602c07a5e710b4c48ed5b3509
Ganesh N. Sivalingam <g.n.sivalingam@gmail.com>
2017-05-26 19:34:14 +0100

    Kind of working

--- process_image.py
+++ process_image.py
@@ -145,7 +145,7 @@ if __name__ == "__main__":
     # video_name = "02_crop_project_video.mp4"
 
     pi = ProcessImage(pt, cal, smooth_window=5)
-    project_output = '170520_0.3.3-no-smoothing_{}'.format(video_name)
+    project_output = '170520_0.3.4-no-smoothing_{}'.format(video_name)
     clip = VideoFileClip("../" + video_name)
     project_clip = clip.fl_image(pi.run_debug)
     project_clip.write_videofile(project_output, audio=False)
